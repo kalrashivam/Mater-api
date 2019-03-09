7acaeec3664475bfd0c1e3ebec3254d0bb85f170
engineersworkbook <shivamkalra2013@gmail.com>
2019-03-09 06:02:54 +0530

    added redis

--- server/server.js
+++ server/server.js
@@ -2,13 +2,21 @@ var express = require("express");
 var bodyparser = require('body-parser');
 var request = require("request");
 var redis = require("redis"),
-    client = redis.createClient();
+    client = redis.createClient(6379, '127.0.0.1');
 
 
 var app = express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
+
+client.on('error',(error) => {
+    console.log('something is wrong with redis the follwing is the error: \n' + error);
+})
+
+client.on('connect', function() {
+    console.log('Redis client connected');
+});
 
 url = "https://tgftp.nws.noaa.gov/data/observations/metar/stations/"
     
@@ -31,18 +39,31 @@ app.get('/metar/ping', (req,res) => {
 });
 
 app.get('/metar/info', (req,res) => {
-    if(req.query.scode){
-        newurl = url + req.query.scode.toUpperCase() + '.TXT';
-        request(newurl, (error,response,body) => {
-            if(error){
-                console.log(error)
-                res.send(error);
-            } else if(body) {
-                res.send(body);
+    //scode -> stores the station query sent by the user
+    scode = req.query.scode;
+    if(scode){
+        check = 0;
+        client.get(scode.toUpperCase(),(err,result) => {
+            if(err) {
+                console.log('error while getting from redis'+ err);
+            } else if (result){
+                res.send(result);
+                check = 1;
+            } else {
+                newurl = url + scode.toUpperCase() + '.TXT';
+                request.get(newurl, (error,response,body) => {
+                    if(error){
+                        console.log(error)
+                        res.send(error);
+                    } else if(body) {
+                        client.set(scode.toUpperCase(),body,'EX',300);
+                        res.send(body);
+                    }
+                    
+                })
             }
-            
-        })
-
+       
+        });
     } else {
         res.send('{"data":"No Scode In Query"}');
     }
